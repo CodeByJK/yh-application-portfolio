8638bbd5eefc79b656a996211ec56fe9ef80fe26
CodeByJK <codebyjk@outlook.com>
2026-02-16 16:31:23 +0100

    Add files via upload
    zoo webbsida

---
+++ ZooWebbsidaProv/script.js
@@ -0,0 +1,9 @@
+function showInfo(name, type, color, description, birthYear) {
+    alert(
+        "Namn: " + name + "\n" +
+        "Typ: " + type + "\n" +
+        "Färg: " + color + "\n" +
+        "Beskrivning: " + description + "\n" +
+        "Födelseår: " + birthYear
+    );
+}
